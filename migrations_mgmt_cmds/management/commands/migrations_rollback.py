c1f1b1f283337b7f3b1c981f985e210187cb2454
Gabriel-Fontenelle <57547647+Gabriel-Fontenelle@users.noreply.github.com>
2022-03-07 13:44:03 -0300

    [Fix] Update rollback to add missing parameters (#5)
    * feat(migrations_rollback): Add missing parameters to `add_arguments`.
    
    * refactor(migrations_rollback): Move code to its own static method to allow for easy overwrite.
    
    * feat(setup): Update version to 0.2.4.
    
    * chore(migrations_rollback): Fix black.

--- migrations_mgmt_cmds/management/commands/migrations_rollback.py
+++ migrations_mgmt_cmds/management/commands/migrations_rollback.py
@@ -37,6 +37,24 @@ class Command(BaseCommand):
             default=DEFAULT_DB_ALIAS,
             help='Nominates a database to synchronize. Defaults to the "default" database.',
         )
+        parser.add_argument(
+            "-v",
+            "--verbosity",
+            action="store",
+            dest="verbosity",
+            default=1,
+            type=int,
+            choices=[0, 1, 2, 3],
+            help="Verbosity level; 0=minimal output, 1=normal output, 2=verbose output, "
+            "3=very verbose output",
+        )
+        parser.add_argument(
+            "--noinput",
+            "--no-input",
+            action="store_false",
+            dest="interactive",
+            help="Tells Django to NOT prompt the user for input of any kind.",
+        )
 
     def handle(self, *args, **options):
         release_path = "{}.json".format(options["release"])
@@ -52,29 +70,11 @@ class Command(BaseCommand):
         self.verbosity = options.get("verbosity")
         self.interactive = options.get("interactive")
 
-        # Get the database we're operating from
-        db = options.get("database")
-        connection = connections[db]
+        # Get the database executor to work out which apps have migrations and which do not
+        executor = self.get_executor(options.get("database"), self.migration_progress_callback)
 
-        # Hook for backends needing any database preparation
-        connection.prepare_database()
-        # Work out which apps have migrations and which do not
-        executor = MigrationExecutor(connection, self.migration_progress_callback)
-
-        # Before anything else, see if there's conflicting apps and drop out
-        # hard if there are any
-        conflicts = executor.loader.detect_conflicts()
-        if conflicts:
-            name_str = "; ".join(
-                "%s in %s" % (", ".join(names), app) for app, names in conflicts.items()
-            )
-            raise CommandError(
-                "Conflicting migrations detected; multiple leaf nodes in the "
-                "migration graph: (%s).\nTo fix them run "
-                "'python manage.py makemigrations --merge'" % name_str
-            )
-
-        plan = executor.migration_plan(targets)
+        # Get the plan from database we're operating from
+        plan = self.get_migration_plan(executor, targets)
 
         if not len(plan):
             raise CommandError("Nothing to rollback")
@@ -96,6 +96,34 @@ class Command(BaseCommand):
             self.stdout.write(self.style.MIGRATE_HEADING("Running migrations:"))
 
         executor.migrate(targets, plan, fake=False, fake_initial=False)
+
+    @staticmethod
+    def get_executor(database, callback=None):
+        # Get the database we're operating from
+        connection = connections[database]
+
+        # Hook for backends needing any database preparation
+        connection.prepare_database()
+
+        # Return the executor from database with callback to process output.
+        return MigrationExecutor(connection, callback)
+
+    @staticmethod
+    def get_migration_plan(executor, targets):
+        # Before anything else, see if there's conflicting apps and drop out
+        # hard if there are any
+        conflicts = executor.loader.detect_conflicts()
+        if conflicts:
+            name_str = "; ".join(
+                "%s in %s" % (", ".join(names), app) for app, names in conflicts.items()
+            )
+            raise CommandError(
+                "Conflicting migrations detected; multiple leaf nodes in the "
+                "migration graph: (%s).\nTo fix them run "
+                "'python manage.py makemigrations --merge'" % name_str
+            )
+
+        return executor.migration_plan(targets)
 
     def migration_progress_callback(self, action, migration=None, fake=False):
         if self.verbosity >= 1:
